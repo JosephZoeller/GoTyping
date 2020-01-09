88490c798c9081c9f58e859b7d07714ad6161c72
JosephZoeller <joe.h.zoeller@gmail.com>
2020-01-08 21:19:08 -0600

    Refactoring into functions
    The runTest() function is separated into distinct components, for organization

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 }
 
 func preface() {
-	fmt.Println("Welcome to my typing speed test. This program will count down from 3, and then you will type words as fast as you can. " +
-		"When you're ready, press the return key to begin.")
+	fmt.Println("Welcome to my typing speed test. This program will count down from 3, and then it will measure how fast you can type words. " +
+		"When you're ready, press the return key to begin and end.")
 	read()
 }
 
@@ -35,18 +35,25 @@ func runTest() {
 		os.Exit(-1)
 	}
 
-	fmt.Println("Seconds to complete:", elapsed.Seconds())
+	printStats(elapsed, countWords(inputString))
+}
 
-	userWords := strings.Split(inputString, " ")
-	var wordCount float64 = 0
-	for i := 0; i < len(userWords); i++ {
-		if userWords[i] != "" {
+func printStats(elapsed time.Duration, wordCount int) {
+	wordCountFl := float64(wordCount)
+	fmt.Println("Seconds to complete:", elapsed.Seconds())
+	fmt.Println("Words per second:", wordCountFl/elapsed.Seconds())
+	fmt.Println("Words per minute:", wordCountFl/elapsed.Seconds()*60)
+}
+
+func countWords(words string) int {
+	wordArray := strings.Split(words, " ")
+	wordCount := 0
+	for i := 0; i < len(wordArray); i++ {
+		if wordArray[i] != "" {
 			wordCount++
 		}
 	}
-	fmt.Println("Words per second:", wordCount/elapsed.Seconds())
-	fmt.Println("Words per minute:", wordCount/elapsed.Seconds() * 60)	
-	fmt.Print("\n")
+	return wordCount
 }
 
 func read() (string, error) {
