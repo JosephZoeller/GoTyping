7612e13d7488bf2747be9fb0f9ae226dda4565f9
JosephZoeller <joe.h.zoeller@gmail.com>
2020-01-08 19:05:57 -0600

    Additional statistics and calculations
    adds number of words typed in the user string and calculates words/second and words/minute

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -11,24 +12,40 @@ func init() {
 }
 
 func main() {
+	preface()
 	countDown(3)
-	beginTest()
+	runTest()
 }
 
-func beginTest() {
+func preface() {
+	fmt.Println("Welcome to my typing speed test. This program will count down from 3, and then you will type words as fast as you can. " +
+		"When you're ready, press the return key to begin.")
+	read()
+}
+
+func runTest() {
 	start := time.Now()
 
 	inputString, readError := read()
 
 	elapsed := time.Since(start)
 
-	if readError == nil {
-		fmt.Println("You wrote: " + inputString) // includes the newline
-		fmt.Printf("It took %s to enter this string.", elapsed)
-	} else {
+	if readError != nil {
 		fmt.Println(readError)
+		os.Exit(-1)
 	}
 
+	fmt.Println("Seconds to complete:", elapsed.Seconds())
+
+	userWords := strings.Split(inputString, " ")
+	var wordCount float64 = 0
+	for i := 0; i < len(userWords); i++ {
+		if userWords[i] != "" {
+			wordCount++
+		}
+	}
+	fmt.Println("Words per second:", wordCount/elapsed.Seconds())
+	fmt.Println("Words per minute:", wordCount/elapsed.Seconds() * 60)	
 	fmt.Print("\n")
 }
 
