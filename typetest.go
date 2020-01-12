5ca1731ca2f4d4235412177b415a30bf4fc3fcd5
JosephZoeller <joe.h.zoeller@gmail.com>
2020-01-11 19:13:23 -0600

    Removing bufio logic
    Cleans project for termbox implementation

--- typetest.go
+++ typetest.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/JosephZoeller/project-0/stringstats"
+	//"github.com/JosephZoeller/project-0/stringstats"
 	"github.com/JosephZoeller/project-0/timer"
+	//"github.com/nsf/termbox-go"
 )
 
 var user *string
 var freestyle *bool
 var duration *string
 
-func init() { //WIP
+func init() {
 	user = flag.String("user", strings.Title(os.Getenv("USER")), "Profile name: defaults to the Operating System's current username.")
 	freestyle = flag.Bool("freestyle", true, "Freestyle testing: the user has no writing prompt and can type whatever they please.")
 	duration = flag.String("duration", "1:00", "Test duration: The length of time that the typing test will last. Format as <Minutes>:<Seconds>")
@@ -24,9 +24,11 @@ func init() { //WIP
 	fmt.Println(*user, *freestyle, *duration)
 }
 
-func preface() { // Intro to the test, prior to it starting.
-	fmt.Printf("Welcome to my typing speed test, %s. This program will count down from 3, and then it will measure how fast you can type words. "+
-		"When you're ready, press the return key to begin and end.\n", *user)
+func preface() {
+	c := 3
+	fmt.Printf("Welcome to my typing speed test, %s. This program will count down from %d, and then it will measure how fast you can type words. "+
+		"When you're ready, press the return key to begin and end.\n", *user, c)
+	// read any key
 	timer.CountDown(3)
 
 }
@@ -34,28 +36,12 @@ func preface() { // Intro to the test, prior to it starting.
 func runTest() {
 	timer.ResetStopWatch()
 	timer.BeginStopWatch()
-	inputString, readError := read() // BUG: If the user starts to write to stdin prior to the countdown ending, it's counted toward the test score/statistics, resulting in unrealistically fast typing speeds.
+	// do test
 	timer.PauseStopWatch()
-	elapsedTime, _ := timer.CheckStopWatch()
-
-	if readError != nil {
-		fmt.Println(readError)
-		os.Exit(-1)
-	}
-
-	printStats(elapsedTime, stringstats.CountWords(inputString))
 }
 
-func printStats(seconds float64, wordCount int) {
-	wordCountFl := float64(wordCount)
-	fmt.Println("Seconds to complete:", seconds)
-	fmt.Println("Words per second:", wordCountFl/seconds)
-	fmt.Println("Words per minute:", wordCountFl/seconds*60)
-}
-
-func read() (string, error) {
-	reader := bufio.NewReader(os.Stdin)
-	reader.Reset(os.Stdin) // not working the way I was hoping it would
-	str, err := reader.ReadString('\n')
-	return str, err
+func printStats() {
+	fmt.Println("Seconds to complete:")
+	fmt.Println("Words per second:")
+	fmt.Println("Words per minute:")
 }
