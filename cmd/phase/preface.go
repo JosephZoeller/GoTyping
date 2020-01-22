ad2caeb08733cc21740f55dad4efc9aac4eceaea
JosephZoeller <joe.h.zoeller@gmail.com>
2020-01-21 19:25:24 -0600

    Slightly rewording the preface

--- cmd/phase/preface.go
+++ cmd/phase/preface.go
@@ -15,13 +15,13 @@ func ShowPreface(username string, free bool) {
 	cd := 3
 	var freestr string
 	if free {
-		freestr = "For this mode, you aren't given a writing prompt. You can write whatever you'd like before the timer runs out." +
+		freestr = "For this mode, you aren't given a writing prompt. You can write whatever you please before the timer runs out." +
 			" During the test, press the Enter key to commit your sentence and begin a new line. You can also press the escape key to end the test."
 	} else {
 		freestr = "For this mode, you are given a writing prompt to copy. You should try to match the prompt exactly." +
 			" During the test, press the Enter key to commit your sentence and recieve a new writing prompt. You can also press the escape key to end the test."
 	}
-	pre := fmt.Sprintf("Welcome to my typing speed test, %s. This program will count down from %d, and then it will measure how fast you can type words. %s"+
+	pre := fmt.Sprintf("Welcome to my typing speed test, %s. This program will count down from %d, and then it'll measure how fast you can type words. %s"+
 		" When you're ready, press any key to begin...", username, cd, freestr)
 	tbutil.Write(0, 0, tbutil.COLDEF, tbutil.COLDEF, pre)
 	tbutil.KeyContinue(false)
